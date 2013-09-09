3f64ae1404305fdd44dc3671212fb0345b08399a
Jamie Sinn <jman.sinn@gmail.com>
2013-09-09 12:41:55 -0400

    Removed unneeded lines

--- BinaryBall/Assets/BehaviorScripts/PlayerMoveBehaviorScript.cs
+++ BinaryBall/Assets/BehaviorScripts/PlayerMoveBehaviorScript.cs
@@ -3,12 +3,7 @@ using System.Collections;
 
 public class PlayerMoveBehaviorScript : MonoBehaviour 
 {
-    public float speed;
-    public float jumpspeed;
-	// Use this for initialization
-	void Start () {
-	
-	}
+    public float speed = 100;
 	
 	// Update is called once per frame
 	void FixedUpdate () 
@@ -18,12 +13,11 @@ public class PlayerMoveBehaviorScript : MonoBehaviour
         
         float horizontalMove = Input.GetAxis("Horizontal");
         float verticalMove = Input.GetAxis("Vertical");
-      //  float jumpMove = Input.GetAxis("Jump");
-       // Vector3 jump = new Vector3(0, jumpMove, 0);
-        Vector3 move = new Vector3(horizontalMove, 0, verticalMove);
+  
+        Vector3 move = new Vector3(horizontalMove, 0.0f, verticalMove);
         //jump = Vector3.up * (jumpspeed * Time.smoothDeltaTime);
 
-        rigidbody.AddForce(move * speed * Time.deltaTime);
+        rigidbody.AddForce(move);
     //    rigidbody.AddForce(jump * jumpspeed * Time.deltaTime);
 	}
 }
